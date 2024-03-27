e8bdfd2dde02c76ce666da2b758997f07531576d
jariaspe <javier.arias.dev@gmail.com>
2024-03-27 12:34:26 +0100

    feat(page-controller): add new package

---
+++ packages/page-controller/index.js
@@ -0,0 +1,1 @@
+export { PageController } from './src/PageController.js';
---
+++ packages/page-controller/src/PageController.js
@@ -0,0 +1,61 @@
+import { ElementController } from '@open-cells/element-controller';
+
+export class PageController extends ElementController {
+  static get BRIDGE_PAGE_PRIVATE_CHANNEL_PREFIX() {
+    return '__bridge_page_';
+  }
+
+  static getPagePrivateChannel(tagName) {
+    return `${PageController.BRIDGE_PAGE_PRIVATE_CHANNEL_PREFIX}${tagName.toLowerCase().replace('-page', '')}`;
+  }
+
+  // Declare properties
+  static get properties() {
+    return {
+      params: { type: Object },
+    };
+  }
+
+  // Initialize properties
+  constructor(host) {
+    super(host);
+    this.params = {};
+  }
+
+  hostConnected() {
+    this.__handleConnections();
+  }
+
+  __handleConnections() {
+    if (this.__hasPageHandlers()) {
+      this.__handlePagePrivateChannel();
+    }
+  }
+
+  __hasPageHandlers() {
+    return !!this.host.onPageEnter || !!this.host.onPageLeave;
+  }
+
+  __handlePagePrivateChannel() {
+    const channelName = PageController.getPagePrivateChannel(this.host.tagName);
+    const wrappedPrivateChannelCallback = this.__wrapPrivateChannelCallback();
+
+    this.subscribe(channelName, wrappedPrivateChannelCallback);
+  }
+
+  __wrapPrivateChannelCallback() {
+    return ({ value: pageStatusValue }) => {
+      const callback = () => {
+        if (pageStatusValue) {
+          this.host.onPageEnter && this.host.onPageEnter();
+        } else {
+          this.host.onPageLeave && this.host.onPageLeave();
+        }
+      };
+
+      if (callback) {
+        callback();
+      }
+    };
+  }
+}
